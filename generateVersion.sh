f48bb97f57e4f963673a74eab7f5844010e495e7
Kevin Dion <kdion@nrao.edu>
2026-07-17 15:13:38 -0400

    Actions-docker make generated version pep440 compliant

--- generateVersion.sh
+++ generateVersion.sh
@@ -7,30 +7,33 @@ project_file=$1
 version="$fallback_version"
 
 ## get the latest version tag, trimming the 'v'
-latest_tag="$(git describe --abbrev=0 --tags --match 'v*.*.*' 2>/dev/null | sed 's/^v//')"
+latest_tag="$(git describe --abbrev=0 --tags --match 'v*.*.*' 2>/dev/null)"
+latest_version="$(echo $latest_tag | sed 's/^v//')"
 if [ -z "$latest_tag" ]
 then
-    latest_tag="$fallback_version"
+    latest_version="$fallback_version"
 fi
 
 ## See if the current commit is tagged
 this_commit_tag="$(git tag -l 'v*.*.*' --points-at HEAD | sed 's/^v//')"
-if [ "$this_commit_tag" = "$latest_tag" ]
+if [ "$this_commit_tag" = "$latest_version" ]
 then
-    version="$latest_tag"
+    version="$latest_version"
 else
-    ## auto-increment patch number from latest_tag
-    latest_major="$(echo $latest_tag | sed -r 's/([[:digit:]]+)\.([[:digit:]]+)\.([[:digit:]]+)/\1/')"
-    latest_minor="$(echo $latest_tag | sed -r 's/([[:digit:]]+)\.([[:digit:]]+)\.([[:digit:]]+)/\2/')"
-    latest_patch="$(echo $latest_tag | sed -r 's/([[:digit:]]+)\.([[:digit:]]+)\.([[:digit:]]+)/\3/')"
+    ## auto-increment patch number from latest_version
+    latest_major="$(echo $latest_version | sed -r 's/([[:digit:]]+)\.([[:digit:]]+)\.([[:digit:]]+)/\1/')"
+    latest_minor="$(echo $latest_version | sed -r 's/([[:digit:]]+)\.([[:digit:]]+)\.([[:digit:]]+)/\2/')"
+    latest_patch="$(echo $latest_version | sed -r 's/([[:digit:]]+)\.([[:digit:]]+)\.([[:digit:]]+)/\3/')"
     next_patch=$((latest_patch + 1))
+
+    dev_count="$(git rev-list ${latest_tag}.. --count)"
 
     next_version="${latest_major}.${latest_minor}.${next_patch}"
     ## dev version - append branch, date and commit hash
     branch="$(git rev-parse --abbrev-ref HEAD | sed 's/[^0-9A-Za-z-]/-/g' | sed 's/--*/-/g')"
     timestamp="$(date '+%Y%m%d%H%M%S')"
     hash="$(git rev-parse --short HEAD)"
-    version="${next_version}-dev-${branch}-${timestamp}+${hash}"
+    version="${next_version}.dev${dev_count}+${branch}-${timestamp}-${hash}"
 fi
 
 echo "$version"
