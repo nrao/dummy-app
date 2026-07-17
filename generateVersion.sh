0f66b6df128099d72e29e13a6a0519b81f9822cd
Kevin Dion <kdion@nrao.edu>
2026-07-17 15:55:03 -0400

    Actions-docker fix generateVersion dev_count

--- generateVersion.sh
+++ generateVersion.sh
@@ -25,7 +25,7 @@ else
     latest_patch="$(echo $latest_version | sed -r 's/([[:digit:]]+)\.([[:digit:]]+)\.([[:digit:]]+)/\3/')"
     next_patch=$((latest_patch + 1))
 
-    dev_count="$(git rev-list vx${latest_version}..HEAD --count 2>/dev/null)"
+    dev_count="$(git rev-list v${latest_version}..HEAD --count 2>/dev/null)"
 
     next_version="${latest_major}.${latest_minor}.${next_patch}"
     ## dev version - append branch, date and commit hash
