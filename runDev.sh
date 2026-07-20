0c6c32d180214402a35b14a7c44c8814d57db2d9
Kevin Dion <kdion@nrao.edu>
2026-07-20 09:17:45 -0400

    App switch to setuptools_scm for dynamic versioning (#26)
    * App switch to setuptools_scm for dynamic versioning
    
    * Try to fix version detection on shallow checkouts
    
    * Pass version to docker build

--- runDev.sh
+++ runDev.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -e
 
-SCRIPT_NAME=/subdir gunicorn --config gunicorn_dev_config.py 'dummy_app:init_app()'
+pip install -e '.[all]'
+
+gunicorn --config gunicorn_dev_config.py 'dummy_app:init_app()'
