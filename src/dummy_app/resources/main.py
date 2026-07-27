b3b35ac5514080f6c1c936170c586fe7138aebff
Kevin Dion <kdion@nrao.edu>
2026-07-27 14:56:36 -0400

    App add additional data to webpage

--- src/dummy_app/resources/main.py
+++ src/dummy_app/resources/main.py
@@ -1,5 +1,7 @@
 import importlib
 import os
+import platform
+import sys
 
 from flask import Blueprint, request
 import flask
@@ -9,10 +11,19 @@ main = Blueprint("main", __name__)
 
 @main.route("/")
 def root():
+    hostname = platform.node()
+    python_version = sys.version
     version = importlib.metadata.version("dummy_app")
     sample_var = os.getenv("SAMPLE_VAR", "UNSET")
 
     color_var = os.getenv("COLOR", "UNSET").lower()
     color_valid = color_var in ("yellow", "orange", "red", "magenta", "violet", "blue", "cyan", "green")
     base_url = request.base_url
-    return flask.render_template("main.html", version=version, base_url=base_url, sample_var=sample_var, color=color_var, color_valid=color_valid)
+    return flask.render_template("main.html",
+                                 hostname=hostname,
+                                 python_version=python_version,
+                                 version=version,
+                                 base_url=base_url,
+                                 sample_var=sample_var,
+                                 color=color_var,
+                                 color_valid=color_valid)
