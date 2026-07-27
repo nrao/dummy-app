e997f6390160ec40f68927917cd721058e55e3b9
Kevin Dion <kdion@nrao.edu>
2026-07-27 14:20:21 -0400

    App add warning when color is invalid (#23)
    * App add warning when color is invalid
    
    * custom yaml for pr preview
    
    * Make it clear values are from PR-specific override

--- src/dummy_app/resources/main.py
+++ src/dummy_app/resources/main.py
@@ -11,11 +11,8 @@ main = Blueprint("main", __name__)
 def root():
     version = importlib.metadata.version("dummy_app")
     sample_var = os.getenv("SAMPLE_VAR", "UNSET")
-    color_var = os.getenv("COLOR", "none").lower()
-    if color_var in ("none", "yellow", "orange", "red", "magenta", "violet", "blue", "cyan", "green"):
-        color = color_var
-    else:
-        color = "invalid"
-    
+
+    color_var = os.getenv("COLOR", "UNSET").lower()
+    color_valid = color_var in ("yellow", "orange", "red", "magenta", "violet", "blue", "cyan", "green")
     base_url = request.base_url
-    return flask.render_template("main.html", version=version, base_url=base_url, sample_var=sample_var, color=color)
+    return flask.render_template("main.html", version=version, base_url=base_url, sample_var=sample_var, color=color_var, color_valid=color_valid)
